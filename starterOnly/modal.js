53e4e5a6cd3179599437e55a604ba860cdce3a30
RpRun <82530937+RpRun@users.noreply.github.com>
2021-10-07 17:24:43 +0200

    return validate() html in comment, reorganisation modal with userValue

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,3 +1,9 @@
+///*
+//  +-+-+-+-+ +-+-+-+-+-+-+-+
+//  |M|E|N|U| |H|A|N|D|L|E|R|
+//  +-+-+-+-+ +-+-+-+-+-+-+-+
+//*/
+
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -7,75 +13,85 @@ function editNav() {
   }
 }
 
-// DOM Elements
+/*
+ +-+-+ +-+-+-+ +-+-+-+-+-+-+-+-+
+ |/|/| |D|O|M| |E|l|e|m|e|n|t|s|
+ +-+-+ +-+-+-+ +-+-+-+-+-+-+-+-+
+*/
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const cross = document.querySelector(".close")
-const submit = document.querySelector(".btn-submit")
-
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
-// launch modal form
-function launchModal() {
-  modalbg.style.display = "block";
-}
-
-// CLOSE MODAL FORM
-const closeModal = () => {
-  modalbg.style.display = "none";
-}
-// add event click to launch closeModal()
-cross.addEventListener('click', closeModal);
-
-// FORM SECTION
-
 const form = document.querySelector('form')
 const inputFirstName = document.querySelector('#first')
 const inputLastName = document.querySelector('#last')
 const inputEmail = document.querySelector('#email')
-
+const inputBirthDate = document.querySelector('#birthdate');
 const inputQuantity = document.querySelector('#quantity')
-
+const inputLocations = document.getElementsByName('location');
 const inputAcceptTermsOfUses = document.querySelector('#checkbox1')
-
 const inputSignInNewsLetter = document.querySelector('#checkbox2')
+const submit = document.querySelector(".btn-submit")
 
+/*
+ +-+-+-+-+-+
+ |T|O|O|L|S|
+ +-+-+-+-+-+
+*/
+const launchModal = () => {
+  modalbg.style.display = "block";
+}
+const closeModal = () => {
+  modalbg.style.display = "none";
+}
 
-// SUBMIT CHECKING
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// add event click to launch closeModal()
+cross.addEventListener('click', closeModal);
+
+/*
+ +-+-+ +-+-+-+-+ +-+-+-+-+-+-+-+
+ |/|/| |F|O|R|M| |S|E|C|T|I|O|N|
+ +-+-+ +-+-+-+-+ +-+-+-+-+-+-+-+
+*/
+let userValue = {}
+
+/*
+ +-+-+ +-+-+-+-+-+-+ +-+-+-+-+-+-+-+-+
+ |/|/| |S|U|B|M|I|T| |C|H|E|C|K|I|N|G|
+ +-+-+ +-+-+-+-+-+-+ +-+-+-+-+-+-+-+-+
+*/
 
 form.addEventListener('submit', (event) => {
+
   // prevent form to be submitted 
   event.preventDefault();
  
-  
-  
 
-// FirstName
+  // FirstName
   if (inputFirstName.value.length > 2) {
-    console.log('ok le prénom:' , inputFirstName.value );
-    // isValid = true;
+    userValue.firstName = inputFirstName.value
   }
   else {
     console.log('Veuillez renseigner votre prénom');
-    // isValid = false;
   }
-// LastName
+
+
+  // LastName
   if (inputLastName.value.length > 2) {
-    console.log('ok le nom:', inputLastName.value);
+   userValue.lastName = inputLastName.value
   }
   else {
     console.log('Veuillez renseigner un nom valide');
   }
 
+
 // MailForm  validation using Regular Expressions
-
-const validateMailForm = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-
-
-if (inputEmail.value.match(validateMailForm)) {
-  console.log('mail ok:', inputEmail.value);
+const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+if (inputEmail.value.match(regex)) {
+  userValue.email = inputEmail.value
 //   alert("Mail ID format is approved!");
 // document.mailForm.mailText.focus();
 // return true;
@@ -87,103 +103,63 @@ if (inputEmail.value.match(validateMailForm)) {
 }
 
 
-// Birthdate validation using Regular Expressions (Regex)
-
-
-const inputBirthDate = document.querySelector('#birthdate');
-
-BirthDateIsValid = /^([0-9]{2})\/([0-9]{2})\/([0-9]{4})$/;
-
-// /^([0-9]{2})\/([0-9]{2})\/([0-9]{4})$/; 
- if (inputBirthDate.value.match(BirthDateIsValid)) {
-   console.log('Date de naissance:' + inputBirthDate.value);
+// Birthdate validation 
+ if (inputBirthDate.value.length>0) {
+   userValue.birthDate = inputBirthDate.value
  } else {
-   console.log('date de naissance pas bon');
+   console.log('Veuillez renseigner votre date de naissance');
  }
-
-// const checkDoB = () =>
-// function checkDoB () {
-
-    // regular expression to match required date format
-    
-    //  /^\d{4}\/\d{1,2}\/\d{1,2}$/;
-
-//     if(inputBirthDate.value != '' && !inputBirthDate.value.match(BirthDateIsValid)) {
-//       alert("Invalid date format: " + inputBirthDate.value);
-//       inputBirthDate.focus();
-//       return false;
-//     }
-//     alert("BOD OK");
-//     return true;
-//   }
-// checkDoB ();
-
- console.log(inputBirthDate.value);
 
 
 // Quantity of tournament participation validation
-const validateQuantity = /^[0-9]/;
-
-if (inputQuantity.value.match(validateQuantity)) {
-  console.log('Nombre de participation(s):', inputQuantity.value);
+if (inputQuantity.value>0||inputQuantity.value<100) {
+  userValue.quantity = inputQuantity.value
 } else {
-  console.log( 'Participation aux précedents tournois:','veuillez selectionner un nombre');
+  console.log( 'Participation aux précédents tournois:','veuillez selectionner un nombre');
 }
 
- // AcceptTermsOfUses
+// inputLocations Checkboxes
+let cities = []
+for (let i=0;i<inputLocations.length;i++) {
+  if(inputLocations[i].checked) {
+    cities.push(inputLocations[i].value)
+  }
+}
+userValue.cities = cities
 
- 
- 
-// for (let i = 0; i < inputAcceptTermsOfUses.length; i++) {
-//   if(inputAcceptTermsOfUses[i].value == "clean") {
-//     inputAcceptTermsOfUses[i].checked = true;
-//     return true
-//    } else {
-//      return false
-//    }
-if (inputAcceptTermsOfUses.checked) 
-console.log('name', inputAcceptTermsOfUses.value);
-// return true
-// } else {
-//   console.log('veuillez accepter les conditions dutilisation', inputAcceptTermsOfUses.value);
-//   return false
-// }
+ // inputTermsOfUses
+if (inputAcceptTermsOfUses.checked) {
+  userValue.cgu = true 
+} else {
+  userValue.cgu = false
+}
 
-
-
-
-console.log(inputSignInNewsLetter.value);
-// }
- 
-
-
-
-
-
-
-
-
- // inputLocations Radios
- const inputLocations = document.getElementsByName('location');
- for (let i=0;i<inputLocations.length;i++) {
-   if(inputLocations[i].value == "clean") {
-    inputLocations[i].checked = true;
-   }
-  console.log(inputLocations[i].checked, inputLocations[i].value)
- }
+checkInput (userValue)
 
 });
 
-// SUBMIT MODAL FORM
-const submitModal = () => {
-  console.log('modal submit')
+const checkInput = (olala) => {
+  console.log('on a un truc là:', olala);
 }
-// add event click to launch submitModal()
-  submit.addEventListener('click', submitModal);
 
 
+// const checkDoB = () => 
+//     if(inputBirthDate.value != '' && !inputBirthDate.value.match(BirthDateIsValid)) 
+//       alert("Invalid date format: " + inputBirthDate.value);
+//       inputBirthDate.focus();
+//       return false;
+//     
+//     alert("BOD OK");
+//     return true;
 
+// checkDoB ();
 
+// SUBMIT MODAL FORM
+// const submitModal = () => {
+//   console.log('modal submit')
+// }
+// // add event click to launch submitModal()
+//   submit.addEventListener('click', submitModal);
 
 
 
